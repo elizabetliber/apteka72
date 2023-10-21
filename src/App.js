596747bc448daa1b767c0940e3ffa31a00ca3af7
lisa <elizabet.liber@gmail.com>
2023-10-22 02:02:02 +0500

    last changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Info from './components/Info';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <Info/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
